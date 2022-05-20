b47371b96291e8d6224aa4bcbdbf6acd6262c864
SpeedBird789 <asifmahmoud787@gmail.com>
2022-05-20 14:56:51 +0100

    end of module 74

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
@@ -8,6 +7,12 @@ import Login from './Pages/Login/Login';
 import RequireAuth from './Pages/Login/RequireAuth';
 import SignUp from './Pages/Login/SignUp';
 import Navbar from './Pages/Shared/Navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import MyAppointments from './Pages/Dashboard/MyAppointments';
+import MyReview from './Pages/Dashboard/MyReview';
+import MyHistory from './Pages/Dashboard/MyHistory';
 
 function App() {
   return (
@@ -17,12 +22,22 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='about' element={<About></About>}></Route>
         <Route path='appointment' element={
-        <RequireAuth>
-          <Appointment></Appointment>
-        </RequireAuth>}></Route>
+          <RequireAuth>
+            <Appointment></Appointment>
+          </RequireAuth>}>
+        </Route>
+        <Route path='dashboard' element={
+            <RequireAuth>
+              <Dashboard></Dashboard>
+            </RequireAuth>}>
+              <Route index element={<MyAppointments></MyAppointments>}></Route>
+              <Route path='review' element={<MyReview></MyReview>}></Route>
+              <Route path='history' element={<MyHistory></MyHistory>}></Route>
+        </Route>
         <Route path='login' element={<Login></Login>}></Route>
         <Route path='signup' element={<SignUp></SignUp>}></Route>
       </Routes>
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
